5a51304a060b8c4c84e3aad9e69d9469e0b7081f
batteryBank <ibanerjee@altimetrik.com>
2019-11-22 16:11:47 +0530

    added for new change

--- src/main/java/com/altimetrik/inventoryManagement/model/ItemInventoryDetails.java
+++ src/main/java/com/altimetrik/inventoryManagement/model/ItemInventoryDetails.java
@@ -12,167 +12,190 @@ import io.swagger.annotations.ApiModelProperty;
  * ItemInventoryDetails
  */
 @Validated
-public class ItemInventoryDetails   {
-  @JsonProperty("vendorId")
-  private Integer vendorId = null;
+public class ItemInventoryDetails {
+	@JsonProperty("vendorId")
+	private Integer vendorId = null;
 
-  @JsonProperty("batteryId")
-  private Integer batteryId = null;
+	@JsonProperty("batteryId")
+	private Integer batteryId = null;
 
-  @JsonProperty("batteryModel")
-  private String batteryModel = null;
+	@JsonProperty("batteryPrice")
+	private Integer batteryPrice = null;
 
-  @JsonProperty("avaliableCount")
-  private Integer avaliableCount = null;
+	@JsonProperty("batteryModel")
+	private String batteryModel = null;
 
-  @JsonProperty("souldCount")
-  private Integer souldCount = null;
+	@JsonProperty("avaliableCount")
+	private Integer avaliableCount = null;
 
-  public ItemInventoryDetails vendorId(Integer vendorId) {
-    this.vendorId = vendorId;
-    return this;
-  }
+	@JsonProperty("souldCount")
+	private Integer souldCount = null;
 
-  /**
-   * vendor id
-   * @return vendorId
-  **/
-  @ApiModelProperty(value = "vendor id")
+	public ItemInventoryDetails vendorId(Integer vendorId) {
+		this.vendorId = vendorId;
+		return this;
+	}
 
+	/**
+	 * vendor id
+	 * 
+	 * @return vendorId
+	 **/
+	@ApiModelProperty(value = "vendor id")
 
-  public Integer getVendorId() {
-    return vendorId;
-  }
+	public Integer getVendorId() {
+		return vendorId;
+	}
 
-  public void setVendorId(Integer vendorId) {
-    this.vendorId = vendorId;
-  }
+	public void setVendorId(Integer vendorId) {
+		this.vendorId = vendorId;
+	}
 
-  public ItemInventoryDetails batteryId(Integer batteryId) {
-    this.batteryId = batteryId;
-    return this;
-  }
+	public ItemInventoryDetails batteryId(Integer batteryId) {
+		this.batteryId = batteryId;
+		return this;
+	}
 
-  /**
-   * batteryId
-   * @return batteryId
-  **/
-  @ApiModelProperty(value = "batteryId")
+	/**
+	 * batteryId
+	 * 
+	 * @return batteryId
+	 **/
+	@ApiModelProperty(value = "batteryId")
 
+	public Integer getBatteryId() {
+		return batteryId;
+	}
 
-  public Integer getBatteryId() {
-    return batteryId;
-  }
+	public void setBatteryId(Integer batteryId) {
+		this.batteryId = batteryId;
+	}
 
-  public void setBatteryId(Integer batteryId) {
-    this.batteryId = batteryId;
-  }
+	public ItemInventoryDetails batteryPrice(Integer batteryPrice) {
+		this.batteryPrice = batteryPrice;
+		return this;
+	}
 
-  public ItemInventoryDetails batteryModel(String batteryModel) {
-    this.batteryModel = batteryModel;
-    return this;
-  }
+	/**
+	 * battery model number
+	 * 
+	 * @return batteryModel
+	 **/
+	@ApiModelProperty(value = "battery Price")
 
-  /**
-   * battery model number
-   * @return batteryModel
-  **/
-  @ApiModelProperty(value = "battery model number")
+	public Integer getBatteryPrice() {
+		return batteryPrice;
+	}
 
+	public void setBatteryPrice(Integer batteryPrice) {
+		this.batteryPrice = batteryPrice;
+	}
 
-  public String getBatteryModel() {
-    return batteryModel;
-  }
+	public ItemInventoryDetails batteryModel(String batteryModel) {
+		this.batteryModel = batteryModel;
+		return this;
+	}
 
-  public void setBatteryModel(String batteryModel) {
-    this.batteryModel = batteryModel;
-  }
+	/**
+	 * battery model number
+	 * 
+	 * @return batteryModel
+	 **/
+	@ApiModelProperty(value = "battery model number")
 
-  public ItemInventoryDetails avaliableCount(Integer avaliableCount) {
-    this.avaliableCount = avaliableCount;
-    return this;
-  }
+	public String getBatteryModel() {
+		return batteryModel;
+	}
 
-  /**
-   * avaliable battery count
-   * @return avaliableCount
-  **/
-  @ApiModelProperty(value = "avaliable battery count")
+	public void setBatteryModel(String batteryModel) {
+		this.batteryModel = batteryModel;
+	}
 
+	public ItemInventoryDetails avaliableCount(Integer avaliableCount) {
+		this.avaliableCount = avaliableCount;
+		return this;
+	}
 
-  public Integer getAvaliableCount() {
-    return avaliableCount;
-  }
+	/**
+	 * avaliable battery count
+	 * 
+	 * @return avaliableCount
+	 **/
+	@ApiModelProperty(value = "avaliable battery count")
 
-  public void setAvaliableCount(Integer avaliableCount) {
-    this.avaliableCount = avaliableCount;
-  }
+	public Integer getAvaliableCount() {
+		return avaliableCount;
+	}
 
-  public ItemInventoryDetails souldCount(Integer souldCount) {
-    this.souldCount = souldCount;
-    return this;
-  }
+	public void setAvaliableCount(Integer avaliableCount) {
+		this.avaliableCount = avaliableCount;
+	}
 
-  /**
-   * sould battery count
-   * @return souldCount
-  **/
-  @ApiModelProperty(value = "sould battery count")
+	public ItemInventoryDetails souldCount(Integer souldCount) {
+		this.souldCount = souldCount;
+		return this;
+	}
 
+	/**
+	 * sould battery count
+	 * 
+	 * @return souldCount
+	 **/
+	@ApiModelProperty(value = "sould battery count")
 
-  public Integer getSouldCount() {
-    return souldCount;
-  }
+	public Integer getSouldCount() {
+		return souldCount;
+	}
 
-  public void setSouldCount(Integer souldCount) {
-    this.souldCount = souldCount;
-  }
+	public void setSouldCount(Integer souldCount) {
+		this.souldCount = souldCount;
+	}
 
+	@Override
+	public boolean equals(java.lang.Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		ItemInventoryDetails itemInventoryDetails = (ItemInventoryDetails) o;
+		return Objects.equals(this.vendorId, itemInventoryDetails.vendorId)
+				&& Objects.equals(this.batteryId, itemInventoryDetails.batteryId)
+				&& Objects.equals(this.batteryModel, itemInventoryDetails.batteryModel)
+				&& Objects.equals(this.avaliableCount, itemInventoryDetails.avaliableCount)
+				&& Objects.equals(this.batteryPrice, itemInventoryDetails.batteryPrice)
+				&& Objects.equals(this.souldCount, itemInventoryDetails.souldCount);
+	}
 
-  @Override
-  public boolean equals(java.lang.Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    ItemInventoryDetails itemInventoryDetails = (ItemInventoryDetails) o;
-    return Objects.equals(this.vendorId, itemInventoryDetails.vendorId) &&
-        Objects.equals(this.batteryId, itemInventoryDetails.batteryId) &&
-        Objects.equals(this.batteryModel, itemInventoryDetails.batteryModel) &&
-        Objects.equals(this.avaliableCount, itemInventoryDetails.avaliableCount) &&
-        Objects.equals(this.souldCount, itemInventoryDetails.souldCount);
-  }
+	@Override
+	public int hashCode() {
+		return Objects.hash(vendorId, batteryId, batteryModel, avaliableCount, souldCount);
+	}
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(vendorId, batteryId, batteryModel, avaliableCount, souldCount);
-  }
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("class ItemInventoryDetails {\n");
 
-  @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("class ItemInventoryDetails {\n");
-    
-    sb.append("    vendorId: ").append(toIndentedString(vendorId)).append("\n");
-    sb.append("    batteryId: ").append(toIndentedString(batteryId)).append("\n");
-    sb.append("    batteryModel: ").append(toIndentedString(batteryModel)).append("\n");
-    sb.append("    avaliableCount: ").append(toIndentedString(avaliableCount)).append("\n");
-    sb.append("    souldCount: ").append(toIndentedString(souldCount)).append("\n");
-    sb.append("}");
-    return sb.toString();
-  }
+		sb.append("    vendorId: ").append(toIndentedString(vendorId)).append("\n");
+		sb.append("    batteryId: ").append(toIndentedString(batteryId)).append("\n");
+		sb.append("    batteryPrice: ").append(toIndentedString(batteryPrice)).append("\n");
+		sb.append("    batteryModel: ").append(toIndentedString(batteryModel)).append("\n");
+		sb.append("    avaliableCount: ").append(toIndentedString(avaliableCount)).append("\n");
+		sb.append("    souldCount: ").append(toIndentedString(souldCount)).append("\n");
+		sb.append("}");
+		return sb.toString();
+	}
 
-  /**
-   * Convert the given object to string with each line indented by 4 spaces
-   * (except the first line).
-   */
-  private String toIndentedString(java.lang.Object o) {
-    if (o == null) {
-      return "null";
-    }
-    return o.toString().replace("\n", "\n    ");
-  }
+	/**
+	 * Convert the given object to string with each line indented by 4 spaces
+	 * (except the first line).
+	 */
+	private String toIndentedString(java.lang.Object o) {
+		if (o == null) {
+			return "null";
+		}
+		return o.toString().replace("\n", "\n    ");
+	}
 }
-
