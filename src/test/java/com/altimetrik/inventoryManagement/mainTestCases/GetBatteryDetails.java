75c93e558108e1e95ddc0a1462303f2c938098d0
Ajit Bhavle <abhavle@altimetrik.com>
2019-11-25 17:54:15 +0530

    added for the change

---
+++ src/test/java/com/altimetrik/inventoryManagement/mainTestCases/GetBatteryDetails.java
@@ -0,0 +1,31 @@
+package com.altimetrik.inventoryManagement.mainTestCases;
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.testng.annotations.Test;
+
+import com.altimetrik.inventoryManagement.baseClass.BaseTest;
+
+public class GetBatteryDetails extends BaseTest {
+			
+			@Test()
+			public void getBatteryDetails() {
+				
+				given().accept("json").contentType("application/json")
+				.when().get("/battery?batteryId=1")//get("http://inventorymanagement.mocklab.io/battery?batteryId=1")
+				.then().assertThat().body("betteryModel", equalTo("amaron")).statusCode(200);
+				
+			}
+
+	/*@Test
+	public void verifyTopLevelURL() {
+		given().when().get("/garage").then()
+		.body("5", equalTo(5));
+		.body("name", equalTo("Ajit"));
+		.body("name",equals("Acme garage"))
+		.body("info.slots",equalsTo(150))
+		.body("info.status",equals("open"))
+		.statusCode(200);
+	}*/
+
+}
